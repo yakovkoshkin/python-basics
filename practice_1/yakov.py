b1da8f2271516a7e137971fe5525c6fba7ef0b8a
yakovkoshkin <yakov.koshkin@gmail.com>
2014-03-03 11:28:05 +0400

    ee

--- practice_1/yakov.py
+++ practice_1/yakov.py
@@ -4,4 +4,4 @@ Created on Mar 3, 2014
 @author: Java Student
 '''
 
-print "Connection Java2 is very slow. Ha-ha-ha, I'm a conflict! Please use Java"
+print "Conn ection Java2 is very slow. Ha-ha-ha, I'm a conflict! Please use Java"
