d2ecb9a6c19902c4321b04922d6bf590e1184e3f
yakovkoshkin <yakov.koshkin@gmail.com>
2014-03-03 11:23:58 +0400

    Artificial Conflict

--- practice_1/yakov.py
+++ practice_1/yakov.py
@@ -4,4 +4,4 @@ Created on Mar 3, 2014
 @author: Java Student
 '''
 
-print "Connection Java2 is very slow. Please use Java"
+print "Connection Java2 is very slow. Ha-ha-ha, I'm a conflict! Please use Java"
